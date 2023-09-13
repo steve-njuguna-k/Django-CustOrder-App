7f6af1c2f70f669e13a9585d966aa4ad55708f3d
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 22:49:09 +0300

    fix: login serializer

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -7,11 +7,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
     """Serializers registration requests and creates a new User."""
 
     password = serializers.CharField(max_length=128, min_length=8, write_only=True)
-    token = serializers.CharField(max_length=255, read_only=True)
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'first_name', 'last_name', 'password', 'token']
+        fields = ['email', 'username', 'first_name', 'last_name', 'password']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
@@ -19,25 +18,29 @@ class RegistrationSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     """Serializers registration requests and logs in an existing User."""
 
-    email = serializers.CharField(max_length=255)
-    username = serializers.CharField(max_length=255, read_only=True)
+    username = serializers.CharField(max_length=255, write_only=True)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
 
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'first_name', 'last_name', 'password', 'token']
+
     def validate(self, data):
-        email = data.get('email', None)
+        username = data.get('username', None)
         password = data.get('password', None)
 
-        if email is None:
-            raise serializers.ValidationError('An email address is required to log in.')
+        if username is None:
+            raise serializers.ValidationError('An username is required to log in.')
 
         if password is None:
             raise serializers.ValidationError('A password is required to log in.')
 
-        user = authenticate(username=email, password=password)
+        user = authenticate(username=username, password=password)
+        refresh = RefreshToken.for_user(user=user)
 
         if user is None:
-            raise serializers.ValidationError('A user with this email and password was not found.')
+            raise serializers.ValidationError('A user with this username and password was not found.')
 
         if not user.is_active:
             raise serializers.ValidationError('This user has been deactivated.')
@@ -47,8 +50,8 @@ class LoginSerializer(serializers.Serializer):
             'first_name': user.first_name,
             'last_name': user.last_name,
             'email': user.email,
-            'refresh': user.tokens['refresh'], 
-            'access': user.tokens['access']
+            'refresh': refresh, 
+            'access': refresh.access_token
         }
 
 class LogoutSerializer(serializers.Serializer):
