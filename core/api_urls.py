cc7a5f980b947a3b33787e6f0f3ee3ee63af14c8
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 20:10:12 +0300

    add items api urls

--- core/api_urls.py
+++ core/api_urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from Customers.views import CustomerAPIView
+from Items.views import ItemAPIView
 
 urlpatterns = [
     path('v1/customers', CustomerAPIView.as_view(), name='list_create_customers'),
     path('v1/customers/<int:pk>', CustomerAPIView.as_view(), name="update_delete_customers"),
+
+    path('v1/items', ItemAPIView.as_view(), name="list_create_items"),
+    path('v1/items/<int:pk>', ItemAPIView.as_view(), name="update_delete_items"),
 ]
