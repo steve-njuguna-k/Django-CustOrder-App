2b07e2cba84e93f0991f592f46f5fc0370170039
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 23:22:24 +0300

    add orders admin

--- Orders/admin.py
+++ Orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+class OderAdmin(admin.ModelAdmin):
+    list_display = ["id", "uuid", "customer", "item", "quantity", "total", "date_created", "date_modified"]
+
+admin.site.register(Order, OderAdmin)
