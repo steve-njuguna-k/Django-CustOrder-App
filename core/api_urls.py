9605ca93ee97ce6ddfe3a9ae70150936fe65e185
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 21:52:33 +0300

    add auth api urls

--- core/api_urls.py
+++ core/api_urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from Customers.views import CustomerAPIView
 from Items.views import ItemAPIView
 from Orders.views import OrderAPIView
+from Auth.views import RegistrationAPIView, LoginAPIView, LogoutAPIView
 
 urlpatterns = [
+    path('v1/login', LoginAPIView.as_view(), name='log_in_user'),
+    path('v1/register', RegistrationAPIView.as_view(), name='register_user'),
+    path('v1/logout', LogoutAPIView.as_view(), name='logout_user'),
+
     path('v1/customers', CustomerAPIView.as_view(), name='list_create_customers'),
     path('v1/customers/<int:pk>', CustomerAPIView.as_view(), name="update_delete_customers"),
 
