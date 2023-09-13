d12a2ffbe1485aadc85b42e4ec07cb951230936e
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 23:21:01 +0300

    add customer admin

--- Customers/admin.py
+++ Customers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
 
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["id", "uuid", "first_name", "last_name", "phone_number", "date_created", "date_modified"]
+
+admin.site.register(Customer, CustomerAdmin)
