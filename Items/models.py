a690b7da7cb32c901199f215729f9d873fd03c13
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 20:04:36 +0300

    add items model

--- Items/models.py
+++ Items/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+SIZES = (
+    ("S", "S"),
+    ("M", "M"),
+    ("L", "L"),
+    ("XL", "XL"),
+    ("XXL", "XXL"),
+)
+
+class Item(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=250, verbose_name='Name')
+    size = models.TextField(verbose_name='Size', choices=SIZES)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
+    date_created = models.DateTimeField(verbose_name='Date Created',auto_now_add=True)
+    date_modified = models.DateTimeField(verbose_name='Date Modified',auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Items'
