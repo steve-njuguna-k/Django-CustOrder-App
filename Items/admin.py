9e9aafc4910a0b4133e3e31b1ef8c55d46571d18
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 23:21:32 +0300

    add items admin

--- Items/admin.py
+++ Items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "uuid", "name", "price", "date_created", "date_modified"]
+
+admin.site.register(Item, ItemAdmin)
