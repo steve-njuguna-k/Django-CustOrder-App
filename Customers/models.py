620d43fe11bef22a80888e78f50f2e7a25c7c853
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 19:44:40 +0300

    add customer model

--- Customers/models.py
+++ Customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Customer(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    first_name = models.CharField(max_length=250, verbose_name='First Name')
+    last_name = models.CharField(max_length=250, verbose_name='Last Name')
+    phone_number = models.CharField(max_length=250, verbose_name='Phone Number')
+    date_created = models.DateTimeField(verbose_name='Date Created',auto_now_add=True)
+    date_modified = models.DateTimeField(verbose_name='Date Modified',auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        verbose_name_plural = 'Customers'
