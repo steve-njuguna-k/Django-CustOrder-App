8dbf53d837971f749751aa5c93d0554c939f7781
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-13 20:15:44 +0300

    add orders api urls

--- core/api_urls.py
+++ core/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from Customers.views import CustomerAPIView
 from Items.views import ItemAPIView
+from Orders.views import OrderAPIView
 
 urlpatterns = [
     path('v1/customers', CustomerAPIView.as_view(), name='list_create_customers'),
@@ -8,4 +9,7 @@ urlpatterns = [
 
     path('v1/items', ItemAPIView.as_view(), name="list_create_items"),
     path('v1/items/<int:pk>', ItemAPIView.as_view(), name="update_delete_items"),
+
+    path('v1/orders', OrderAPIView.as_view(), name="list_create_orders"),
+    path('v1/orders/<int:pk>', OrderAPIView.as_view(), name="update_delete_orders"),
 ]
