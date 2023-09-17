8b2fb8b56b01b69e6c2a354f779dd14b30aa12f5
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-17 22:56:21 +0300

    add comments to orders api view

--- Orders/views.py
+++ Orders/views.py
@@ -21,69 +21,83 @@ class OrderAPIView(APIView):
         try:
             serializer = self.serializer_class(data=request.data)
             if serializer.is_valid(raise_exception=True):
+                # Save the serialized data to create a new Order
                 serializer.save()
 
+                # Retrieve the customer associated with the order
                 customer = Customer.objects.get(id=serializer.data['customer'])
                 
+                # Send an SMS notification to the customer about the order
                 send_sms(
                     "{} {}".format(serializer.data['customer_first_name'], serializer.data['customer_last_name']), 
                     serializer.data['item_name'], 
                     serializer.data['quantity'], 
                     serializer.data['total'],
                     customer.phone_number
-                    )
-                return JsonResponse({"status":status.HTTP_201_CREATED, "message": "Order Created Successfully! An SMS has been sent to the customer for delivery", "results": serializer.data})
+                )
+                return JsonResponse({"status": status.HTTP_201_CREATED, "message": "Order Created Successfully! An SMS has been sent to the customer for delivery", "results": serializer.data})
             else:
-                return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": serializer.errors})
+                # Handle validation errors and return a response with error details
+                return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": serializer.errors})
         except Exception as e:
-            return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": {"error": str(e)}})
+            # Handle unexpected exceptions and return an error response
+            return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": {"error": str(e)}})
 
     def get(self, request, pk=None):
         """GET request function to either retrieve a given Order object or list all Orders objects"""
 
         if pk:
-            # if the order's primary key is part of the request
+            # If the order's primary key is part of the request, retrieve a single Order
             try:
                 order_obj = get_object_or_404(Order, id=pk)
-                serializer =self.serializer_class(order_obj)
+                serializer = self.serializer_class(order_obj)
                 if serializer.is_valid:
-                    return JsonResponse({"status":status.HTTP_200_OK, "message": "Order Details Retrieved Successfully!", "results": serializer.data})
+                    return JsonResponse({"status": status.HTTP_200_OK, "message": "Order Details Retrieved Successfully!", "results": serializer.data})
                 else:
-                    return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": serializer.errors})
+                    # Handle validation errors and return a response with error details
+                    return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": serializer.errors})
             except Exception as e:
-                return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": {"error": str(e)}})
+                # Handle unexpected exceptions and return an error response
+                return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": {"error": str(e)}})
         else:
-            # if the order's primary key is not part of the request
+            # If the order's primary key is not part of the request, list all Orders
             try:
                 order_objs = Order.objects.all().order_by("-date_created")
-                serializer =self.serializer_class(order_objs, many=True, context={'request': request})
+                serializer = self.serializer_class(order_objs, many=True, context={'request': request})
                 if serializer.is_valid:
-                    return JsonResponse({"status":status.HTTP_200_OK, "message": "Orders Retrieved Successfully!", "results": serializer.data})
+                    return JsonResponse({"status": status.HTTP_200_OK, "message": "Orders Retrieved Successfully!", "results": serializer.data})
                 else:
-                    return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": serializer.errors})
+                    # Handle validation errors and return a response with error details
+                    return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": serializer.errors})
             except Exception as e:
-                return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": {"error": str(e)}})
+                # Handle unexpected exceptions and return an error response
+                return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": {"error": str(e)}})
 
     def patch(self, request, pk=None):
         """PATCH request function to update a given Order object"""
 
         try:
             order_obj = get_object_or_404(Order, id=pk)
-            serializer =self.serializer_class(instance=order_obj, data=request.data, partial=True)
+            serializer = self.serializer_class(instance=order_obj, data=request.data, partial=True)
             if serializer.is_valid(raise_exception=True):
+                # Save the updated Order details
                 serializer.save()
-                return JsonResponse({"status":status.HTTP_200_OK, "message": "Order Details Updated Successfully!", "results": serializer.data})
+                return JsonResponse({"status": status.HTTP_200_OK, "message": "Order Details Updated Successfully!", "results": serializer.data})
             else:
-                return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": serializer.errors})
+                # Handle validation errors and return a response with error details
+                return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": serializer.errors})
         except Exception as e:
-            return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": {"error": str(e)}})
+            # Handle unexpected exceptions and return an error response
+            return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": {"error": str(e)}})
         
-    def delete(self, pk=None):
+    def delete(self, request, pk=None):
         """DELETE request function to delete a given Order object"""
 
         try:
             order_obj = get_object_or_404(Order, id=pk)
+            # Delete Order object
             order_obj.delete()
-            return JsonResponse({"status":status.HTTP_204_NO_CONTENT, "message": "Order Details Deleted Successfully!"})
+            return JsonResponse({"status": status.HTTP_204_NO_CONTENT, "message": "Order Details Deleted Successfully!"})
         except Exception as e:
-            return JsonResponse({"status":status.HTTP_400_BAD_REQUEST, "message": "An Error Occured!", "results": {"error": str(e)}})
+            # Handle unexpected exceptions and return an error response
+            return JsonResponse({"status": status.HTTP_400_BAD_REQUEST, "message": "An Error Occurred!", "results": {"error": str(e)}})
