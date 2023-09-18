e2d03506e3be6a0068ebea37fd7ddb8fe6caffd3
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-18 08:54:52 +0300

    fix: change from SQLite to PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -91,9 +91,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
