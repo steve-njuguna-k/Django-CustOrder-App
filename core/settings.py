e24816bed01571e9aa7a6c84ee006eca4df85d45
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-19 09:37:52 +0300

    add all static files in staticfile_build

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images) settings
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
