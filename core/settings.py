41220262ef859c9f2f5cf29b033cf1a14f80c09d
steve-njuguna-k <stevenjuguna93@gmail.com>
2023-09-20 10:05:21 +0300

    fix: edit config.yml

--- core/settings.py
+++ core/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
+        "HOST": "127.0.0.1",
         "PORT": os.environ.get("DB_PORT"),
     }
 }
